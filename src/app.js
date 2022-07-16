0330fb05badb70b88f5fdff1a1d74afafc630f0e
vyshnav-chikku <vyshnavk891@gmail.com>
2022-07-16 14:20:23 +0530

    commit 2

--- src/app.js
+++ src/app.js
@@ -32,9 +32,10 @@ require("./db/conn");
 const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV == "production") {
-  app.use(express.static("client/build"));
-  app.get("*", function (req, res) {
-    res.sendFile("index.html");
+  const path = require("path");
+  app.use(express.static(path.join("client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve("client/build", "index.html"));
   });
 }
 
